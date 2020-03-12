6cf2a8c6ae1d9063f7dd9510311962014ebd8504
Sergio Escalante <macuser@SergioEscalante.local>
2020-03-12 15:22:41 -0400

    Add comments to code

--- morse.py
+++ morse.py
@@ -1,15 +1,19 @@
+#import re and sys
 import re
 import sys
 
+#create morse-code encoder/decoder function
 def morse(text):
+    #introduce all letters with their respected morse value.
     encrypt = {
         'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..',
         'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
         'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.==', 'X': '-..-', 'Y': '-.--', 'Z': '--..', ' ': '.....'
     }
-
+    #iterate encrypt elements to decrytp each morse code.
     decrypt = {value: key for key, value in encrypt.items()}
 
+    #RegEx for input to check if we need to decrypt or encrypt
     if re.match(r'(\s|-|\.)+', text):
         return ''.join(decrypt[i] for i in text.split())
     return ' '.join(encrypt[i] for i in text.upper())
